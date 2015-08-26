6e88b0014ce352409f8fd298858ceef7a51a3683
Aaron Schlesinger <arschles@gmail.com>
2015-08-26 13:27:44 -0700

    fixing image cmd

--- wrapper.sh
+++ wrapper.sh
@@ -6,8 +6,8 @@ case "$1" in
   static) docker run --rm -v $PWD:/app -w /app treeder/go static ;;
   vendor) docker run --rm -v $PWD:/app -w /app treeder/go vendor ;;
   image) \
-    if [ -z "$1" ]; then
-      echo "missing image name"
+    if [ -z "$2" ]; then
+      echo "Missing image name"
       exit 1
     fi
     docker run --rm -v $PWD:/app -w /app -v /var/run/docker.sock:/var/run/docker.sock treeder/go image $1
