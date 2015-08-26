41fafbaff0e3c7f8dd271a459f82a80bed88ebc6
Aaron Schlesinger <arschles@gmail.com>
2015-08-26 13:25:00 -0700

    adding simple wrapper script

---
+++ wrapper.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+case "$1" in
+  build) docker run --rm -v $PWD:/app -w /app treeder/go build ;;
+  cross) docker run --rm -v $PWD:/app -w /app treeder/go cross ;;
+  static) docker run --rm -v $PWD:/app -w /app treeder/go static ;;
+  vendor) docker run --rm -v $PWD:/app -w /app treeder/go vendor ;;
+  image) \
+    if [ -z "$1" ]; then
+      echo "missing image name"
+      exit 1
+    fi
+    docker run --rm -v $PWD:/app -w /app -v /var/run/docker.sock:/var/run/docker.sock treeder/go image $1
+    ;;
+esac
+exit 0
