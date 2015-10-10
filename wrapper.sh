52a46a56cc16251fea6f22c745c5fb25669b427b
Jose Diaz-Gonzalez <josegonzalez@users.noreply.github.com>
2015-10-10 13:27:37 -0400

    Reformat for readability

--- wrapper.sh
+++ wrapper.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
 case "$1" in
-  build) docker run --rm -v $PWD:/app -w /app treeder/go build ;;
-  cross) docker run --rm -v $PWD:/app -w /app treeder/go cross ;;
-  static) docker run --rm -v $PWD:/app -w /app treeder/go static ;;
-  vendor) docker run --rm -v $PWD:/app -w /app treeder/go vendor ;;
-  run) docker run --rm -v $PWD:/app -w /app -p 8080:8080 iron/base ./app ;;
-  run-static) docker run --rm -v $PWD:/app -w /app -p 8080:8080 iron/base ./static ;;
+  build) 
+    docker run --rm -v $PWD:/app -w /app treeder/go build 
+    ;;
+  cross) 
+    docker run --rm -v $PWD:/app -w /app treeder/go cross 
+    ;;
+  static) 
+    docker run --rm -v $PWD:/app -w /app treeder/go static 
+    ;;
+  vendor) 
+    docker run --rm -v $PWD:/app -w /app treeder/go vendor 
+    ;;
+  run) 
+    docker run --rm -v $PWD:/app -w /app -p 8080:8080 iron/base ./app 
+    ;;
+  run-static) 
+    docker run --rm -v $PWD:/app -w /app -p 8080:8080 iron/base ./static 
+    ;;
   image) \
     if [ -z "$2" ]; then
       echo "Missing image name"
@@ -14,7 +26,9 @@ case "$1" in
     fi
     docker run --rm -v $PWD:/app -w /app -v /var/run/docker.sock:/var/run/docker.sock treeder/go image $1
     ;;
-  *) echo "Invalid command"
-    exit 1 ;;
+  *) 
+    echo "Invalid command"
+    exit 1 
+    ;;
 esac
 exit 0
