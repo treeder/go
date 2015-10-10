6a7734cf4706b530c82e8ceaa690ddfeb1314037
Jose Diaz-Gonzalez <josegonzalez@users.noreply.github.com>
2015-10-10 13:36:23 -0400

    Wrap script in a main method
    Runnable scripts should have their logic in a main method

--- wrapper.sh
+++ wrapper.sh
@@ -1,38 +1,41 @@
 #!/usr/bin/env bash
 set -eo pipefail; [[ $TRACE ]] && set -x
 
-case "$1" in
-  build)
-    docker run --rm -v "$PWD":/app -w /app treeder/go build
-    ;;
-  cross)
-    docker run --rm -v "$PWD":/app -w /app treeder/go cross
-    ;;
-  static)
-    docker run --rm -v "$PWD":/app -w /app treeder/go static
-    ;;
-  vendor)
-    docker run --rm -v "$PWD":/app -w /app treeder/go vendor
-    ;;
-  fmt)
-    docker run --rm -v "$PWD":/app -w /app treeder/go fmt
-    ;;
-  run)
-    docker run --rm -v "$PWD":/app -w /app -p 8080:8080 iron/base ./app
-    ;;
-  run-static)
-    docker run --rm -v "$PWD":/app -w /app -p 8080:8080 iron/base ./static
-    ;;
-  image)
-    if [[ -z "$2" ]]; then
-      echo "Missing image name"
+main() {
+  case "$1" in
+    build)
+      docker run --rm -v "$PWD":/app -w /app treeder/go build
+      ;;
+    cross)
+      docker run --rm -v "$PWD":/app -w /app treeder/go cross
+      ;;
+    static)
+      docker run --rm -v "$PWD":/app -w /app treeder/go static
+      ;;
+    vendor)
+      docker run --rm -v "$PWD":/app -w /app treeder/go vendor
+      ;;
+    fmt)
+      docker run --rm -v "$PWD":/app -w /app treeder/go fmt
+      ;;
+    run)
+      docker run --rm -v "$PWD":/app -w /app -p 8080:8080 iron/base ./app
+      ;;
+    run-static)
+      docker run --rm -v "$PWD":/app -w /app -p 8080:8080 iron/base ./static
+      ;;
+    image)
+      if [[ -z "$2" ]]; then
+        echo "Missing image name"
+        exit 1
+      fi
+      docker run --rm -v "$PWD":/app -w /app -v /var/run/docker.sock:/var/run/docker.sock treeder/go image $1
+      ;;
+    *)
+      echo "Invalid command"
       exit 1
-    fi
-    docker run --rm -v "$PWD":/app -w /app -v /var/run/docker.sock:/var/run/docker.sock treeder/go image $1
-    ;;
-  *)
-    echo "Invalid command"
-    exit 1
-    ;;
-esac
-exit 0
+      ;;
+  esac
+}
+
+main "$@"
